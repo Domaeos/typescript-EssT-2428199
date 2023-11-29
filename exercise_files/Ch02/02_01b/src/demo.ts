825387f296bed4cf9401ac734b46b5f11207f3d6
Domaeos <ryanmurphy.ic@icloud.com>
2023-11-29 16:32:13 +0000

    Further changes

---
+++ exercise_files/Ch02/02_01b/src/demo.ts
@@ -0,0 +1,19 @@
+let x: number = 5;
+let y: string;
+let z : boolean;
+let a: Date;
+
+// let b: any;
+// b = 1234
+// b = "hi";
+// b = true;
+// Use casting instead
+
+let b: string[];
+b = "Hello" as any;
+// perfectly legitimate, however should be able to express exact type you want and not use any
+
+
+
+
+
--- exercise_files/Ch02/02_Challenge/app.ts
+++ exercise_files/Ch02/02_Challenge/app.ts
@@ -1,24 +1,38 @@
-const todoItems = [
-    { id: 1, title: "Learn HTML", status: "done", completedOn: new Date("2021-09-11") },
-    { id: 2, title: "Learn TypeScript", status: "in-progress" },
-    { id: 3, title: "Write the best app in the world", status: "todo" },
+enum TodoStatus {
+    Done = "done",
+    InProgress = "in-progress",
+    Todo = "todo"
+
+}
+
+interface TodoItem {
+    id: number;
+    title: string;
+    completedOn?: Date;
+    status: TodoStatus
+}
+
+const todoItem: TodoItem[] = [
+    { id: 1, title: "Learn HTML", status: TodoStatus.Done, completedOn: new Date("2021-09-11") },
+    { id: 2, title: "Learn TypeScript", status: TodoStatus.InProgress },
+    { id: 3, title: "Write the best app in the world", status: TodoStatus.Todo },
 ]
 
-function addTodoItem(todo) {
-    const id = getNextId(todoItems)
+function addTodoItem(todo: string): TodoItem {
+    const id = getNextId(todoItem)
 
-    const newTodo = {
+    const newTodo: TodoItem = {
         id,
         title: todo,
-        status: "todo",
+        status: TodoStatus.Todo,
     }
 
-    todoItems.push(newTodo)
+    todoItem.push(newTodo)
 
     return newTodo
 }
 
-function getNextId(items) {
+function getNextId<T extends { id: number }>(items: T[]): number {
     return items.reduce((max, x) => x.id > max ? x.id : max, 0) + 1
 }
 
